dccb61f3339d578486475fd805b5010f2a0518d9
ericabasak <ericabasak@gmail.com>
2021-02-14 12:42:55 -0500

    native string search practice problem

---
+++ native_string_search.js
@@ -0,0 +1,20 @@
+// native string search practice problem
+
+// pseudocode
+// loop over the longer string
+// loop over the shorter string
+// if the characters dont match, break out of the inner loop
+// if the characters do match, keep going
+// if you complete the inner loop and find a match, incrememnt the number of matches
+// return the count
+
+// my solution
+function stringSearch(str1, str2) {
+
+}
+
+
+// a correct solution
+function stringSearch(str1, str2) {
+
+}
--- search_binary.js
+++ search_binary.js
@@ -40,3 +40,18 @@ function binarySearch(arr, val) {
 };
 
 binarySearch([1,4,5,9,11], 5)
+
+// a solution like above but a bit shorter
+function binarySearch(arr, elem) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end)/ 2);
+  while(arr[middle] !== elem && start <= end) {
+    if(elem < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor(start + end)/ 2;
+  }
+  return arr[middle] === elem ? middle : -1;
+}
+
+binarySearch([1,4,5,9,11], 5)
