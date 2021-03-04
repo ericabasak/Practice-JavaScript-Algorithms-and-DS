715fb941041be80baf1492d76570f07e77d63498
ericabasak <ericabasak@gmail.com>
2021-03-04 06:59:13 -0500

    selection sort practice problem made changes to

--- selectionSort.js
+++ selectionSort.js
@@ -12,9 +12,18 @@ function selectionSort(arr) {
     let lowest = i;
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] < arr[lowest]) {
-        
+        lowest = j;
       }
+    }
+    // here's the swapping part
+    if(i !== lowest){
+      var temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
     }
   }
   return arr;
 }
+
+selectionSort([34,22,10,19,17])
+
