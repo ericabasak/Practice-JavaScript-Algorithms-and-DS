25e515644b89243de3ee173271cb921366d1256f
ericabasak <ericabasak@gmail.com>
2021-05-06 10:18:30 -0400

    reverse a string option 2

--- reverseAString.js
+++ reverseAString.js
@@ -1,11 +1,22 @@
 // reverse a string
 
-function reverse(s) {
-  let o = "";
-  for (let i = s.length - 1; i >=0; i--)
-      o += s[i];
-      return o;
+// option 1
+// function reverse(s) {
+//   let o = "";
+//   for (let i = s.length - 1; i >=0; i--)
+//       o += s[i];
+//       return o;
+// }
+
+// console.log(reverse("boo"));
+// console.log(reverse("erica"));
+
+// option 2
+function reverse(str) {
+  let o = [];
+  for (let i = 0, len = str.length; i <= len; i++)
+  o.push(str.charAt(len - i));
+  return o.join('');
 }
 
-console.log(reverse("boo"));
 console.log(reverse("erica"));
