cea642a358c70748f2f8922a3f3de64fcb25406a
ericabasak <ericabasak@gmail.com>
2021-05-06 10:28:08 -0400

    reverse a string option 3

--- reverseAString.js
+++ reverseAString.js
@@ -1,22 +1,30 @@
 // reverse a string
 
-// option 1
+// OPTION 1
 // function reverse(s) {
 //   let o = "";
 //   for (let i = s.length - 1; i >=0; i--)
 //       o += s[i];
 //       return o;
 // }
-
 // console.log(reverse("boo"));
 // console.log(reverse("erica"));
 
-// option 2
+
+
+// OPTION 2
 function reverse(str) {
   let o = [];
   for (let i = 0, len = str.length; i <= len; i++)
   o.push(str.charAt(len - i));
   return o.join('');
 }
-
 console.log(reverse("erica"));
+
+
+
+// OPTION 3
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
+
