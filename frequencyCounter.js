b1ba5503f70798bc1c0ad99e24893f9e62e2195c
ericabasak <ericabasak@gmail.com>
2021-05-08 16:18:15 -0400

    frequency counter practive problem 2

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -4,6 +4,8 @@
 // has its corresponding value squared in the second array.
 // the frequency of values must be the same.
 
+
+// OPTION 1 
 function same(arr1, arr2) {
   if (arr1.length !== arr2.length){
     return false;
@@ -21,3 +23,27 @@ function same(arr1, arr2) {
 console.log(same())
 
 // O(Nsquared)
+
+// OPTION 2
+function same(arr1, arr2) {
+  if(arr1.length !== arr2.length){
+    return false;
+  }
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let val of arr1){
+    frequencyCounter1[arr1] = (frequencyCounter1[val] || 0) + 1
+  }
+  for (let val of arr2){
+    frequencyCounter2[arr2] = (frequencyCounter2[val] || 0 ) + 1
+  }
+  for (let key of frequencyCounter1){
+    if(!(key ** 2 in frequencyCounter2)){
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
+      return false;
+    }
+  }
+  return true;
+}
