70a737e46715b3c1143714756a840f524453e66f
ericabasak <ericabasak@gmail.com>
2021-05-08 22:59:20 -0400

    anagram practice problem

---
+++ anagrams.js
@@ -0,0 +1,13 @@
+// anagrams
+
+// given two strings wirte a function to determine if
+// the second string is an anagram of the first. 
+// an anagram is a word, phrase, or name formed by rearrangine the
+// letters of another. ie. cinema/icemand
+
+function anagram(str1,str2) {
+  if (str1.length !== str2.lenght){
+    return false;
+  }
+  
+}
