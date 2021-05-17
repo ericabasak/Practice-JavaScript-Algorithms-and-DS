093ba57e0336685b05cc9b578344bc62373a6806
ericabasak <ericabasak@gmail.com>
2021-05-17 17:13:16 -0400

    more practice with frequency counter practice problem

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -47,3 +47,48 @@ function same(arr1, arr2) {
   }
   return true;
 }
+
+// frequency counter again
+function same(arr1, arr2){
+  if (arr1.length !== arr2.length){
+    return false
+  }
+
+  let counter1 = {}
+  let counter2 = {}
+
+  for(let val of arr1){
+    counter1[val] = (counter[val] || 0) + 1
+  }
+
+  for(let val of arr2){
+    counter2[val] = (counter2[val] || 0) + 1
+  }
+
+  for(let key in counter1){
+    if(!(key ** 2 in counter2)) {
+      return false
+    }
+    if(counter2[key ** 2] !== counter1[key]){
+      return false
+    }
+  }
+  return true
+}
+console.log(same([1,2,3], [1,9,4]))
+
+// same as above
+function same(arr1, arr2){
+  if(arr1.length !== arr2.length){
+    return false
+  }
+  for (let i = 0; i > arr1.length; i++){
+    let bestIndex = arr2.indexOf(arr1[i] ** 2)
+    if(bestIndex === -1){
+      return false
+    }
+    arr2.splice(bestIndex, 1)
+  }
+  return true
+}
+
--- rubyPractice.rb
+++ rubyPractice.rb
@@ -5,3 +5,48 @@ def greet(name)
 end
 
 greet("baby")
+
+arr = [1,2,7,0,55]
+
+arr.each {|elem| puts elem}
+
+arr = [1,2,7,0,55]
+
+arr.each_index {|elem| puts elem, "---"}
+
+arr = [1,2,7,0,55]
+
+arr.each_index {|elem| puts elem, "***"}
+
+arr = [1,2,7,0,55]
+
+arr.each_with_index {|elem, index| puts "index: #{index} is #{elem} in the arr"}
+
+
+
+arr = ["cat", "dog"]
+
+arr.map {|animal| puts animal}
+
+arr = ["cat", "dog"]
+
+arr.map! {|animal| puts animal}
+
+
+
+
+(1..10).select do |item|
+  puts item.even?
+end
+
+(1..10).select do |item|
+  item.even?
+end
+
+(1..10).select do |item|
+  item.odd?
+end
+
+(0..10).select do |num, index|
+  puts "index: #{index} is #{num.even?}"
+end
