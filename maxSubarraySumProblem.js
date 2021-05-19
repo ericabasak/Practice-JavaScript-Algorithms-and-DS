bf921d75d9f8ccedc0907f5580d6b418f81eacea
ericabasak <ericabasak@gmail.com>
2021-05-19 15:47:16 -0400

    same as last commit

--- maxSubarraySumProblem.js
+++ maxSubarraySumProblem.js
@@ -73,3 +73,42 @@ function maxSubarraySum(arr, num) {
   }
   return max;
 }
+console.log(maxSubarraySum([4,7], 3))
+
+
+// again
+function maxSubarraySum(arr, num) {
+  if(num > arr.length){
+    return null;
+  }
+  let max = 0;
+  for(let i = 0; i < arr.length - num + 1; i++){
+    let temp = 0;
+    for(let j = 0; j < num; j++){
+      temp += arr[i+j];
+    }
+    if(temp > max){
+      max = temp;
+    }
+  }
+  return max;
+}
+console.log(maxSubarraySum([1,2,3,4,5,6,7,8,9,10], 3))
+
+// again
+function maxSubarraySum(arr, num){
+  if(num > arr.length){
+    return null;
+  }
+  let max = 0;
+  for (let i = 0; i < arr.length - max + 1; i++){
+    let temp = 0;
+    for (let j = 0; j < num; j++){
+      temp += arr[i+j];
+    }
+    if(temp > max){
+      max = temp;
+    }
+  }
+  return max
+}
