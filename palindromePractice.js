32b6d5a70af997416ed87ade5195c5fb16e13b37
ericabasak <ericabasak@gmail.com>
2021-06-05 12:01:55 -0400

    practice with palindrome

--- palindromePractice.js
+++ palindromePractice.js
@@ -1,10 +1,25 @@
-function isPalindrome(str){
-  for(let i = 0; i < str.length/2; i++){
+// function isPalindrome(str){
+//   for(let i = 0; i < str.length/2; i++){
+//     if(str[i] !== str[str.length - 1 - i]){
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+// console.log(isPalindrome("racecar"));
+// console.log(isPalindrome("hello"));
+
+// palindrome practice problem
+// reads the same forwards and backwards 
+
+function isPalindrome(str) {
+  for(let i = 0; i < str.length; i++){
     if(str[i] !== str[str.length - 1 - i]){
       return false;
     }
   }
   return true;
 }
-console.log(isPalindrome("racecar"));
-console.log(isPalindrome("hello"));
+
+console.log(isPalindrome("mamam"));
+console.log(isPalindrome("mamjm"));
