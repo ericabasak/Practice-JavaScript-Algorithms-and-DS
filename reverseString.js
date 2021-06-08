24275bc813bcdb215d9ea90cf77f34ff7ebbb630
ericabasak <ericabasak@gmail.com>
2021-06-07 21:41:50 -0400

    reversing a string practice problem

---
+++ reverseString.js
@@ -0,0 +1,32 @@
+// reverse a string without using reverse method
+
+function reverseAString(str) {
+  let newRevString = "";
+  for(let i = str.length - 1; i >= 0; i--){
+    newRevString = newRevString + str[i];
+  }
+  return newRevString;
+}
+console.log(reverseAString("erica"));
+console.log(reverseAString("alphabet"));
+
+function reverse(str){
+  let newStr = "";
+  for(let i = str.length - 1; i >= 0; i--){
+    newStr += newStr + str[i];
+  }
+}
+console.log(reverseAString("erica"));
+console.log(reverseAString("alphabet"));
+
+// again
+
+function reverseAString(str){
+  let newStr = "";
+  for(let i = str.length - 1; i >= 0; i--){
+    newStr += str[i];
+  }
+  return newStr;
+}
+console.log(reverseAString("monster"));
+console.log(reverseAString("freedom"));
