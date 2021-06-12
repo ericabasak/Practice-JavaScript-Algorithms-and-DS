9ca76bd7ff6e9b644edeef8f599f0d728ed03cd1
ericabasak <ericabasak@gmail.com>
2021-06-11 23:58:58 -0400

    frequency count problem

--- findMostRepeated.js
+++ findMostRepeated.js
@@ -7,9 +7,10 @@
 // see how many times that an number repeats itself
 // return the number that shows up the most
 function findMostRepeated(arr) {
+  if(arr.length == 0) return null;
   let obj = {};
   for(let i = 0; i > arr.lenght; i++){
-    
+    if()
   }
 }
 console.log(findMostRepeated([3,3,4,4,4,5,6,6,6,1,1,4,4,4,1,1,1,1,1,4,4,4,4,6]));
