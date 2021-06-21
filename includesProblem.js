c6c8da56e0b6cef4570ae275f70b682b01d79cc4
ericabasak <ericabasak@gmail.com>
2021-06-21 10:05:01 -0400

    changes in includes problem file

--- includesProblem.js
+++ includesProblem.js
@@ -1,12 +1,34 @@
-// includes
+// includes method on string
+// practicing with how the includes method works
 
-function includesAString(word, letter){
+function includeString(word, letter){
   let matches;
   for(let i = 0; i < word.length; i++){
     if(word[i] === letter){
       matches = true;
     }
-  }
-  return !!matches;
+  } return !!matches;
 }
-console.log(includesAString("monkey", "k"))
+console.log(includeString("starbucks", "b"))
+console.log(includeString("macintosh", "x"))
+
+
+
+// sorting a string
+function inOrder(str){
+  return str.split("").sort().join("");
+}
+console.log(inOrder("alphabet"));
+
+// sorting again
+function sortString(str){
+  return str.split("").sort().join("-");
+}
+console.log(sortString("mocha"))
+console.log(sortString("starbucks"));
+
+function sortString(str){
+  return str.split("").sort().join("*");
+}
+console.log(sortString("mocha"))
+console.log(sortString("starbucks"));
