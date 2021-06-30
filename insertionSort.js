316c7187b71a33a8308eeddb2c4b97017e83b3f9
ericabasak <ericabasak@gmail.com>
2021-06-29 22:54:40 -0400

    practicing with insertion sort problem

--- insertionSort.js
+++ insertionSort.js
@@ -1,2 +1,16 @@
 // insertion sort practice problem
 
+// insertion sort psuedocode
+// start by picking the second element in the array.
+// now compare the second element with the element before it and swap if necessary.
+// continue to the next element and if it is not in the correct order, iterate through the sorted portion to place the element in the correct order.
+// repeat until the array is sorted.
+
+function insertionSort(arr){
+  for(let i = 0; i < arr.length; i++){
+    let currentVal = arr[i];
+  }
+  return arr;
+}
+console.log(insertionSort([7,99,39,0,12,29]));
+
