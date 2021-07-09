014d7e999494d284f4d589d46358a52a07af2311
ericabasak <ericabasak@gmail.com>
2021-07-08 23:00:05 -0400

    find the index of the location of greatest number given in an array

---
+++ findMaxNum.js
@@ -0,0 +1,15 @@
+// HOMEWORK, find max integer in array (Return its position in the array)
+
+// given an array of positive integers find the number with the biggest value
+// return the index at which the greated integer is located in the array
+
+
+arr = [2,7,6,4,11,4,5,4]
+
+function findMaxNumber(arr){
+  
+  for(let i = 0; i < arr.length; i++){
+
+  }
+  return
+}
