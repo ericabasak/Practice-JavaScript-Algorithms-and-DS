129a107ef26d3e1a8606f12ce360071e7e87e734
ericabasak <ericabasak@gmail.com>
2021-07-11 13:25:51 -0400

    working on merg sort with two sorted arrays

---
+++ mergeSortTwoArrays.js
@@ -0,0 +1,19 @@
+// merge two sorted arrays
+
+// both arrays are sorted
+
+// psuedocode for merging two aorted arrays
+// look through each array
+// compare each number and begin to put numbers in a new 
+// array with numbers from least to greated values
+
+// groceryItemsFromWholeFoods
+let arr1 = [1,2,6,7,8];
+// groceryItemsFromWholeFoods
+let arr2 = [3,4,5,9,10];
+
+function findMin(arr1, arr2){
+  let minArr1 = arr1[0];
+  let minArr2 = arr2[0];
+  
+}
