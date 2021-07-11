787705725680e5d39da63683162f014841d1fa22
ericabasak <ericabasak@gmail.com>
2021-07-11 12:06:23 -0400

    questions about finding index in arr

--- findMaxNumberIndex.js
+++ findMaxNumberIndex.js
@@ -10,17 +10,23 @@ arr = [2,7,6,4,11,4,5,4]
 // go through each element in the array
 // find the max integer and returns its index
 
+// QUESTIONS
+// ***************************************
+// QUESTION - IF I REMOVE COUNTER IT STILL WORKS
+// *****************************************
+// instead if using indexOf() how does this work under the hood?
+
 function findMaxNumberIndex(maxNumber, arr){
-  let counter = 0;
+  // let counter = 0;
   for(let i = 0; i < arr.length; i++){
     let currentNumber = arr[i];
     if(currentNumber === maxNumber){
-      counter++;
+      // counter++;
     }
   }
-  if(counter === 0) return -1;
+  // if(counter === 0) return -1;
   return arr.indexOf(maxNumber);
 }
-console.log(findMaxNumberIndex(11, [2,7,6,4,11,4,5,4]));
+console.log(findMaxNumberIndex(7, [2,7,6,4,11,4,5,4]));
 console.log(findMaxNumberIndex(57, [2,7,6,4,11,4,5,4]));
-console.log(findMaxNumberIndex(5, [2,7,6,4,11,4,5,4]));
+console.log(findMaxNumberIndex(6, [2,7,6,4,11,4,5,4]));
