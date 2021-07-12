f8e39e7b7ef2fa1381c12afb94bf77e1d7fc7b8f
ericabasak <ericabasak@gmail.com>
2021-07-12 12:07:11 -0400

    working on linear search problem to find index of a given value

--- linearSearchProblem.js
+++ linearSearchProblem.js
@@ -5,6 +5,41 @@
 // equal to the value. if it is, return the index at which the element
 // is found. else, return -1.
 
+// function linearSearch(arr, val){
+//   for(let i = 0; i < arr.length; i++){
+//     if(arr[i] === val){
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+
+// console.log(linearSearch([1,3,6,9,11], 9))
+// console.log(linearSearch([1,3,6,9,11], 4))
+
+// linear time O(n)
+
+// ************************************************
+// write a function called linearSearch , which accepts an array and a value
+//  and returns the index at which the value exists or return -1 if it does not exist
+// DO NOT USE INDEXOF() method
+
+
+function linearSearch(arr, val){
+  for(let i = 0; i < arr.length; i++){
+      if(arr[i] === val){
+          return i;
+      }
+  }
+  return -1;
+}
+console.log(linearSearch([10, 15,20,25], 25));
+console.log(linearSearch([10, 15,20,25,30,35], 15));
+console.log(linearSearch([10, 15,20,25], 13));
+
+
+// again
+
 function linearSearch(arr, val){
   for(let i = 0; i < arr.length; i++){
     if(arr[i] === val){
@@ -13,8 +48,4 @@ function linearSearch(arr, val){
   }
   return -1;
 }
-
-console.log(linearSearch([1,3,6,9,11], 9))
-console.log(linearSearch([1,3,6,9,11], 4))
-
-// linear time O(n)
+console.log(linearSearch([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16], 13));
