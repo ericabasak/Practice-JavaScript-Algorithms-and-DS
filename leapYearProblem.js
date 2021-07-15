c141c070be952a6f2f0d3cd60c4384c2d390def0
ericabasak <ericabasak@gmail.com>
2021-07-15 14:14:29 -0400

    leap year problem

---
+++ leapYearProblem.js
@@ -0,0 +1,12 @@
+// Write a program that takes no parameter and returns a list (or array) 
+// the next 20 leap years (note that to solve this you need to know at least one past leap year).
+// ie. leapYears() = [2024, 2028, 2032, 2036, 2040, 2044, ...etc]
+
+
+// psuedocode
+// takes no parameters
+// 
+function leapYears(){
+
+}
+console.log(leapYears([]))
