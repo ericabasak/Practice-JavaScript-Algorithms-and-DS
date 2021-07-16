9942926cf3dad61a012e1cc9c3b90ee1eb553035
ericabasak <ericabasak@gmail.com>
2021-07-16 16:29:57 -0400

    reverse an array problem

---
+++ reverseArrayProblem.js
@@ -0,0 +1,17 @@
+// reverse an array
+// i.e. [4,3,6,7,2,7,5,6,7,1] = [1,7,6,5,7,2,7,6,3,4]
+
+
+// swap first element in array with last element in the array
+// then move inward from both ends until reach center of array
+// return reversed array
+
+function reverseArr(arr){
+  for(let i = 0; i < arr.length; i++){
+    for(let j = j + 1; j < arr.length - i - 1 ;j++){
+      
+    }
+  }
+  return arr;
+}
+console.log(reverseArr())
