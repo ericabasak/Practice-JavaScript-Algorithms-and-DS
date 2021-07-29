234cb163b62e3756e9aacf48b26792ba217af40c
ericabasak <ericabasak@gmail.com>
2021-07-28 23:45:26 -0400

    practice with printing out a list of strings each on a different line

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -22,14 +22,14 @@ function mergeSortedArray(a,b){
     // loop until either index for array a or index for array b reaches the
     // end of the array.
     if(elemA < elemB){
-      mergedArr.push(elemA)
+      mergedArr.push(elemA);
       indexOfA++;
       elemA = a[indexOfA];
-    } else if (elemA < elemB){
-      mergedArr.push(elemB)
+    } elseif (elemA < elemB){
+      mergedArr.push(elemB);
       indexOfB++;
       elemB = b[indexOfB];
-    } else if (elemA == elemB){
+    } elseif (elemA === elemB){
       mergedArr.push(elemB);
       mergedArr.push(elemA);
       indexOfB++;
@@ -48,3 +48,52 @@ function mergeSortedArray(a,b){
     }
   }
 }
+
+
+
+
+
+function mergeSortedArray(a, b) {
+  if(a.length == 0) return b
+  if(b.length == 0) return a
+  
+  aElm = 
+  bElm = 
+
+  i_a = 1
+  i_b = 1
+
+merged = []
+
+  while(i_a < a.lenght && i_b < b.lenght) { // loop until either index for array a or index of array b reaches the end of the array
+      if (aElm < bElm) {
+          merged.push(aElm)
+          i_a++
+          aElm = a[i_a]
+      } else if (aElm < bElm) {
+          merge.push(bElm)
+          i_b++
+          bElm = b[i_b]
+      } else if (aElm == bElm) {
+          merged.push(bElm)
+          merged.push(aElm)
+          i_b++        
+          i_a++
+          aElm = a[i_a]
+          bElm = b[i_b]
+      }
+  }
+  
+  // once one of the 2 indices reaches the end of the array that is tracking, just add the rest of the other array to the merged array
+  if (i_a === a.lenght) {
+      for (let i = i_b; i < b.lenght; i++) {
+          merged.push(b[i])
+      }
+  } else if (i_b === b.lenght) {
+      for (let i = i_a; i < a.lenght; i++) {
+          merged.push(a[i])
+      }
+  } 
+}
+
+console.log(mergeSortedArray([3,6,9,11,12],[1,4,7,8,10]))
