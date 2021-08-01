19f7624f4f2a986a5728c120067b8fdafbfbec67
ericabasak <ericabasak@gmail.com>
2021-08-01 18:35:02 -0400

    trying to work through problem to frame words with *

--- stringsProblem.js
+++ stringsProblem.js
@@ -1,4 +1,5 @@
-// Write a function that takes a list of strings and prints them, one per line, in a rectangular frame. 
+// Write a function that takes a list of strings and prints them, one per line, 
+// in a rectangular frame. 
 // For example the list ["Hello", "World", "in", "a", "frame"] gets printed as:
 // *********
 // * Hello *
@@ -16,7 +17,35 @@
 // but you must add white space to the words to create a box?
 // it must create a box around all words
 
-function funFrame(arr, str){
-  
+
+
+function funFrame(arr){
+  // first thing would be to take the array and separate each string by the (,)
+  // arr.split(",")
+  let text = "";
+  for(let x of arr){
+    text += x;
+    // text.join("*" + x + "*");
+    console.log(x)
+  }
 }
-console.log(funFrame())
+console.log(funFrame(["Hello", "World", "in", "a", "frame"]))
+
+// find the longest word
+
+function makeFrame(arr){
+  let maxWord = 0;
+  let currentWord = 0;
+  for(let i = 0; i < arr.length; i++){
+    console.log(arr[i])
+    for(let j = 0; j < arr.length; j++){
+      // for each maxWord add (* ) in front of word, add ( *) at the end of the word  
+      // for all other words currentWord then you have to add (* ) in front of word, then 
+      // take the difference between the maxWord and currentWord, and put ( ) to make the difference up, 
+      // followed by ( *) at the end
+      
+    }
+
+  }
+}
+console.log(makeFrame(["Hello", "World", "in", "a", "frame"]))
