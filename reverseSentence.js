e4c09608e995ec2de0fb45278d8a3dcee687b15a
ericabasak <ericabasak@gmail.com>
2021-08-03 21:52:01 -0400

    working code for reserving a string of words

--- reverseSentence.js
+++ reverseSentence.js
@@ -1,7 +1,24 @@
 // reverse a sentence. ie. "today I had some pasta" -> "pasta some had I today"
 
+// doesnt work
+// function reverseSentence(str){
+//   let reversedStr = "";
+//   reversedStr.split(" ");
+//   for(let i = str.length - 1; i >= 0; i--){
+//     reversedStr.push(str[i]);
+//   }
+//   return reversedStr;
+// }
+// console.log(reverseSentence("today I had some pasta"));
+// console.log(reverseSentence("i'm going to rock these interviews"))
+
 function reverseSentence(str){
-  
+  let arr = str.split(" ");
+  let result = [];
+  for(let i = arr.length -1; i >= 0; i--){
+    result.push(arr[i]);
+  }
+  return result.join(" ");
 }
 console.log(reverseSentence("today I had some pasta"));
-console.log(reverseSentence("i'm going to rock these interviews"))
+console.log(reverseSentence("i'll do great in my interview"))
