a5ef3f2faf8eb488ad2d7a888b16a08d55a8e9a6
ericabasak <ericabasak@gmail.com>
2021-08-15 09:28:17 -0400

    first attempt at fizzbuzz problem

---
+++ fizzBuzz.js
@@ -0,0 +1,18 @@
+// The goal of this function is to print out all numbers from 1 to 100 but 
+// with three exceptions:
+// For every number that is divisible by 3 and 5, console log "FizzBuzz".
+// For every number that is divisible by only 3 and not 5, console log "Fizz".
+// For every number that is divisible by only 5 and not 3, console .log "Buzz".
+
+function fizzBuzz(){
+  for(let i = 1; i <= 100; i++){
+    if(i % 3 && i % 5 === 0){
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0){
+      console.log("Fizz");
+    } else if (i % 5 === 0){
+      console.log("Buzz");
+    }
+  }
+}
+console.log(fizzBuzz())
