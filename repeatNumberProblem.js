3091822dcb38c105165440574da03716d1ffd57a
ericabasak <ericabasak@gmail.com>
2021-08-23 21:24:18 -0400

    find most repeated number in array

--- repeatNumberProblem.js
+++ repeatNumberProblem.js
@@ -1,2 +1,7 @@
 // given an array of numbers, find the most repeated number and return it
 // ie. [3,4,5,4,4,4,1,1,6,6,6,1,1,1,4,4,4,4,4] , in this example the most repeated number is 4
+
+function noRepeats(){
+
+}
+console.log(noRepeats());
