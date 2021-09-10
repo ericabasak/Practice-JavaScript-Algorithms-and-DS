3025b10490364c28ee527a44e0340adc6f0e0aa3
ericabasak <ericabasak@gmail.com>
2021-09-09 21:25:38 -0400

    working on finding the most repeated number in an array

--- repeatNumberProblem.js
+++ repeatNumberProblem.js
@@ -1,7 +1,26 @@
 // given an array of numbers, find the most repeated number and return it
 // ie. [3,4,5,4,4,4,1,1,6,6,6,1,1,1,4,4,4,4,4] , in this example the most repeated number is 4
 
-function noRepeats(){
-
+// my first attempt
+function numRepeats(arr){
+  let currentNum = 0;
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === arr[i])
+      let currentNum = arr[i];
+      arr[i]++
+  } 
 }
-console.log(noRepeats());
+console.log(numRepeats());
+
+// second attempt
+function mostRepeatedNum(arr){
+  let obj = {};
+  for(let i = 0; i < arr.length; i++){
+    let elem = arr[i];
+    if(obj[elem] === undefined){
+      obj[elem] = 1;
+    } else {
+      obj[elem += 1]
+    }
+  }
+}
